2b322e4060462168dcc3953241fd7b38296a840b
onokatio <onokatio@gmail.com>
2021-09-26 17:15:59 +0900

    update

---
+++ README.md
@@ -0,0 +1,87 @@
+# 高専シラバス一括ダウンロード
+
+自分が居た学年とその西暦を入れると、その時に受けた授業のシラバスのPDFを一括ダウンロードします。
+国立高専のみ対応しています。
+
+## 使い方
+
+### 0. 依存するパッケージをインストールする。
+
+このプログラムはLinuxかMacでしか動作しません。
+依存するコマンドは以下です。
+
+- zsh (シェル)
+- pup (HTMLのスクレイピング)
+- pdfunite (popplerプロジェクトの一部で、PDFを連結する)
+
+大抵の場合homebrewとかapt-get(apt)とかでインストールできると思います。
+
+#### Mac & Homebrew
+
+```
+brew instal zsh pup poppler
+```
+
+
+#### Ubuntu
+
+```
+sudo apt install zsh
+sudo apt install poppler-utils
+```
+
+Ubuntuにはpupパッケージが提供されていないため、GithubのReleaseからシングルバイナリを落としてきてパスの通った場所に置きましょう。
+
+https://github.com/ericchiang/pup
+
+### 1. 西暦と学年を定義する
+
+main.shの下の部分に、以下のような記述があります。
+
+```
+getPDF 2016 1
+getPDF 2017 2
+getPDF 2019 3
+getPDF 2020 4
+getPDF 2021 5
+joinAllPDF
+```
+
+このgetPDFコマンドは、第一引数に西暦、第二引数に学年を取ります。ここを任意の数字に編集してください。
+また、留年した場合は同じ学年を複数回書いても構いません。
+
+1年と2年でそれぞれ`回留年した場合の例:
+```
+getPDF 2016 1
+getPDF 2016 2
+getPDF 2017 3
+getPDF 2016 4
+```
+
+### 2. 学校番号を入力する
+
+main.shの最上部に、以下のような記述があります。
+
+```
+SCHOOL=14
+DEPARTMENT=14
+```
+
+これは学校番号と学科番号です。
+この数字はシラバスのサイト( https://syllabus.kosen-k.go.jp/Pages/PublicSchools ) から飛べる各校のページのURLから確認できます。
+例えば、木更津高専 情報工学学科のURLは https://syllabus.kosen-k.go.jp/Pages/PublicSubjects?school_id=14&department_id=14&year=2021&lang=ja となるため、学校番号が14、学科番号が14だということがわかります。
+自分の番号がわかったら編集してください。
+
+### 3. 実行する
+
+ファイルが編集できたら、zshでシェルスクリプトを実行してください。
+
+```
+zsh ./main.sh
+```
+
+## 出力について
+
+このプログラムは、まずシラバスのPDFを `./pdfs/西暦-学年/` というフォルダにダウンロードします。
+その後、それらのPDFを一つにまとめたPDFを `./pdfs/西暦-学年.pdf` という名前で生成します。
+最後に、さらにそれらを含めた完全なPDFを `./all.pdf` として生成します。
--- main.sh
+++ main.sh
@@ -1,11 +1,14 @@
 set -eu
 
+SCHOOL=14
+DEPARTMENT=14
+
 function getSubjectIds(){
 	YEAR="$1"
 	GRADE="$2"
 	COL1="$(( 6+(4*(GRADE-1)) ))"
 	COL2="$(( COL1 + 2 ))"
-	curl -Ss "https://syllabus.kosen-k.go.jp/Pages/PublicSubjects?school_id=14&department_id=14&year=${YEAR}&lang=ja" | pup 'table#sytablenc tbody tr json{}' | jq -r ".[4:][] | {\"sub\": .children[2], \"a\": .children[${COL1}], \"b\": .children[${COL2}]} | select(.a.text or .b.text) | .sub.children[0].children[0].href" | sed -e 's/\&amp;/\&/g' | sed -r 's/.+subject_id=(.+)\&year=(.+)\&.+/\1 \2/'
+	curl -Ss "https://syllabus.kosen-k.go.jp/Pages/PublicSubjects?school_id=${SCHOOL}&department_id=${DEPARTMENT}&year=${YEAR}&lang=ja" | pup 'table#sytablenc tbody tr json{}' | jq -r ".[4:][] | {\"sub\": .children[2], \"a\": .children[${COL1}], \"b\": .children[${COL2}]} | select(.a.text or .b.text) | .sub.children[0].children[0].href" | sed -e 's/\&amp;/\&/g' | sed -r 's/.+subject_id=(.+)\&year=(.+)\&.+/\1 \2/'
 }
 
 function getPDFUrls(){
@@ -13,23 +16,25 @@ function getPDFUrls(){
 		do
 			id=`echo $line | cut -d ' ' -f 1`
 			year=`echo $line | cut -d ' ' -f 2`
-			echo "https://syllabus.kosen-k.go.jp/Pages/SyllabusPDF?school_id=14&department_id=14&subject_id=${id}&year=${year}&lang=ja&subject_code=&preview=False&type=start&attachment=true"
+			echo "https://syllabus.kosen-k.go.jp/Pages/SyllabusPDF?school_id=${SCHOOL}&department_id=${DEPARTMENT}&subject_id=${id}&year=${year}&lang=ja&subject_code=&preview=False&type=start&attachment=true"
 		done
 }
 
 function getPDF(){
-	mkdir -p "./pdfs/$2"
-	getPDFUrls "$1" "$2" | while read line;do wget --show-progress -nv -P "./pdfs/$2/" --content-disposition "$line";done
-	pdfunite ./pdfs/$2/*.pdf ./pdfs/$2.pdf
+	mkdir -p "./pdfs/$1-$2"
+	getPDFUrls "$1" "$2" | while read line;do wget --show-progress -nv -P "./pdfs/$1-$2/" --content-disposition "$line";done
+	pdfunite ./pdfs/$1-$2/*.pdf ./pdfs/$1-$2.pdf
 }
 
 function joinAllPDF(){
-	pdfunite "./pdf/*.pdf" "./all.pdf"
+	pdfunite ./pdfs/*.pdf ./all.pdf
 }
+
+rm -rf ./pdfs
 
 getPDF 2016 1
 getPDF 2017 2
-getPDF 2018 3
-getPDF 2019 4
-getPDF 2020 5
+getPDF 2019 3
+getPDF 2020 4
+getPDF 2021 5
 joinAllPDF
